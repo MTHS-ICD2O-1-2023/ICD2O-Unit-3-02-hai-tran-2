fb3d940ef4e0d8d3fcae6d7f58dfa828d21784fa
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-03 18:38:22 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a trapezoid.
+ */
+function calculateVolume() {
+  // input
+  const lengthOfPyramid = parseFloat(document.getElementById('length-of-pyramid').value)
+  const widthOfPyramid = parseFloat(document.getElementById('width-of-pyramid').value)
+  const heightOfPyramid = parseFloat(document.getElementById('height-of-pyramid').value)
+
+  // process
+  const volumeOfPyramid = (lengthOfPyramid * widthOfPyramid * heightOfPyramid) / 3
+
+  // output
+  document.getElementById('answer').innerHTML = "The volume is: " + volumeOfPyramid.toFixed(2) + " mm³"
+}
